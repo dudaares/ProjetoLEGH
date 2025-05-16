4f83f129eeda881169123baa2bd6df0ba210418b
dudaares <dudaares@gmail.com>
2025-05-16 17:57:06 -0300

    nps config

---
+++ Assets/Sprites/TilesetPrototipo/Trolls/TileMas/Tileset/TilesetsAroc1 TSX/Catalogo de npcs.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Catalogo de npcs" tilewidth="32" tileheight="32" tilecount="540" columns="20">
+ <image source="../../Art/TilesetArco1/Catalogo de npcs.png" width="640" height="880"/>
+ <tile id="6">
+  <animation>
+   <frame tileid="0" duration="400"/>
+   <frame tileid="1" duration="400"/>
+  </animation>
+ </tile>
+</tileset>
